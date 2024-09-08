cb81f9a9ce284fba3c7547f08a725fbe94346069
CarlosCCQ <jhan.ccanto.q@uni.pe>
2024-09-08 11:18:10 -0500

    actualizacion de  add.js,  utilizacion de docker compose, actualizacion de ciac.yml  e inclusion de prometheus.yml

--- Semana1/Actividad1-CC3S2/devops-practice/src/app.js
+++ Semana1/Actividad1-CC3S2/devops-practice/src/app.js
@@ -1,8 +1,21 @@
 const express = require('express'); //Se guarda en express el modulo importado Express, este es un framework para Node.js
 const app = express(); //Se llama a la funcion express, que inicializa una nueva aplicacion Express, esta aplicacion se asigna a la constante 'app'
 
-app.get('/',(req,res) => { //Define la ruta GET en la raiz ('/') que responde con 'Hello, World!'
+app.get('/', (req, res) => { //Define la ruta GET en la raiz ('/') que responde con 'Hello, World!'
     res.send('Hello, World!'); //Envia la respuesta 'Hello, World!' al cliente cuando se accede a la ruta raiz ('/') 
 });
 
-module.exports = app; //Exporta la aplicacion 'app' para que pueda ser utilizada en otros modulos de Node.js
+app.get('/delay', (req, res) => {
+    setTimeout(() => {
+        res.send('This was delayed by 2 seconds');
+    }, 2000);
+});
+
+module.exports = app;//Exporta la aplicacion 'app' para que pueda ser utilizada en otros modulos de Node.js
+
+if(require.main === module) {
+    const port = process.env.PORT || 0;
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
--- Semana1/Actividad1-CC3S2/devops-practice/tests/app.test.js
+++ Semana1/Actividad1-CC3S2/devops-practice/tests/app.test.js
@@ -1,20 +1,64 @@
-const request = require('supertest');//Import la libreria 'supertest', que se utiliza para realizar pruebas en aplicaciones web HTTP 
-const app = require('../src/app');//Se importa Express desde app.js de la carpeta src
+// const request = require('supertest');//Import la libreria 'supertest', que se utiliza para realizar pruebas en aplicaciones web HTTP 
+// const app = require('../src/app');//Se importa Express desde app.js de la carpeta src
 
-let server; //Se utiliza para almacenar la instancia del servidor Express, esta se iniciara al ejecutar las pruebas y finalizara al finalizar las pruebas
+// let server; //Se utiliza para almacenar la instancia del servidor Express, esta se iniciara al ejecutar las pruebas y finalizara al finalizar las pruebas
 
-beforeAll((done) => {
-    server = app.listen(done); //Para iniciar el servidor en un puerto dinamico
+// beforeAll((done) => {
+//     server = app.listen(done); //Para iniciar el servidor en un puerto dinamico
+// });
+
+// afterAll((done) => {
+//     server.close(done); //Para cerrar el servidor despues de todas las pruebas
+// });
+
+// describe('GET /',() => {//Se esta agrupando un conjunto de pruebas para la ruta 'GET /'
+//     it('should return Hello, World!', async () => {//Se define el test, el primer argumento es lo que deberia hacer el codigo, el segundo argumento es la logica de la prueba asincronica
+//         const res = await request(app).get('/');//await espera a que se complete la solicitud HTTP
+//         expect(res.statusCode).toEqual(200);//Se verifica el estado HTTP en la respuesta
+//         expect(res.text).toBe('Hello, World!');//Aqui se verifica que res.text sea exactamente 'Hello, World!'
+//     });
+// });
+
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('GET /', () => {
+    let server;
+
+    beforeAll(() => {
+        sever = app.listen(0); // Usar 0 permite al sistema asignar un puerto libre automatico
+    });
+
+    afterAll(() => {
+        server.close(); // Cierra el servidor despues de que las pruebas hayan terminado
+    });
+
+    it('should return Hello, World!', async () => {
+        const res = await request(app).get('/');
+        expect(res.statusCode).toEqual(200);
+        expect(res.text).toBe('Hello, World!');
+    });
 });
 
-afterAll((done) => {
-    server.close(done); //Para cerrar el servidor despues de todas las pruebas
-});
+describe('GET /delay', () => {
+    let server;
 
-describe('GET /',() => {//Se esta agrupando un conjunto de pruebas para la ruta 'GET /'
-    it('should return Hello, World!', async () => {//Se define el test, el primer argumento es lo que deberia hacer el codigo, el segundo argumento es la logica de la prueba asincronica
-        const res = await request(app).get('/');//await espera a que se complete la solicitud HTTP
-        expect(res.statusCode).toEqual(200);//Se verifica el estado HTTP en la respuesta
-        expect(res.text).toBe('Hello, World!');//Aqui se verifica que res.text sea exactamente 'Hello, World!'
+    beforeAll(() => {
+        server = app.listen(0);
+    });
+
+    afterAll(() => {
+        server.close();
+    });
+
+    it('should return after a delay', async () => {
+        const start = Date.now();
+        const res = await request(app).get('/delay');
+        const end = Date.now();
+        const duration = end - start;
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.text).toBe('This was delayed by 2 seconds');
+        expect(duration).toBeGreaterThanOrEqual(2000);
     });
 });
